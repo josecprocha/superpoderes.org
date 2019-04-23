b2d88d42c8ee098f7ffd09eb2766822008db08bd
José Cândido Pereira da Rocha <josecprocha@gmail.com>
2019-04-22 23:29:46 -0300

    Create error pages using blade template.

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,34 @@
+<html lang="pt-br">
+  <head>
+    <title>Página Não Encontrada</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{ url('/css/error.css') }}">
+  </head>
+
+  <body class="antialiased font-sans">
+    <div class="md:flex min-h-screen">
+      <div class="w-full md:w-1/2 bg-white flex items-center justify-center">
+        <div class="max-w-sm m-8">
+          <div class="text-black text-5xl md:text-15xl font-black">
+            403
+          </div>
+          <div class="w-16 h-1 bg-purple-light my-3 md:my-6"></div>
+          <p class="text-grey-darker text-2xl md:text-3xl font-light mb-8 leading-normal">
+            Sinto muito, você não está autorizado a acessar esta página.
+          </p>
+          <a href="{{ url('/') }}">
+            <button class="bg-transparent text-grey-darkest font-bold uppercase tracking-wide py-3 px-6 border-2 border-grey-light hover:border-grey rounded-lg">
+              Voltar pra Página Inicial
+            </button>
+          </a>
+        </div>
+      </div>
+      <div class="relative pb-full md:flex md:pb-0 md:min-h-screen w-full md:w-1/2">
+        <div style="background-image: url('{{ url('/svg/403.svg') }}');" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
