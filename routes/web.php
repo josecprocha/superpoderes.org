6ecf20aac591df965ff6ed7388faafa4fee22897
José Cândido Pereira da Rocha <josecprocha@gmail.com>
2019-04-22 23:27:22 -0300

    Create the routes for the first four test pages: home, html5, hanoi and cubnpup.

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,29 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+/*
+|--------------------------------------------------------------------------
+| "test" pages
+|--------------------------------------------------------------------------
+|
+| Pages designed to find out what works on the devices children have
+| available.
+|
+*/
+
+Route::get('/test', function () {
+  return view('test.home'); // Beginning
+});
+
+Route::get('/test/html5', function () {
+  return view('test.html5.home'); // Game Select
+});
+
+Route::get('/test/html5/hanoi', function () {
+  return view('test.html5.games.hanoi');
+});
+
+Route::get('/test/html5/cubnpup', function () {
+  return view('test.html5.games.cubnpup');
+});
