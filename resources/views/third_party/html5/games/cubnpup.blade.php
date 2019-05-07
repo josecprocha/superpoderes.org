92c6b7f12b47f4f6f96335c3815fe2e74c1cdfe1
José Cândido Pereira da Rocha <josecprocha@gmail.com>
2019-05-07 14:08:42 -0300

    Include Hanoi and CubNPup license info in their blade template.

--- resources/views/third_party/html5/games/cubnpup.blade.php
+++ resources/views/third_party/html5/games/cubnpup.blade.php
@@ -1,5 +1,25 @@
-<!-- https://codepen.io/desandro/pen/ezNawy -->
-<!-- http://cubnpup.com/ -->
+<!--
+A Pen created at CodePen.io. You can find this one at https://codepen.io/desandro/pen/ezNawy.
+
+How to play: Drag cub to star, Drag grid to rotate.
+
+Also available at [cubnpup.com](http://cubnpup.com)
+
+This is a proof-of-concept for a game. Basic art, no sound, no options, no polish. But the core game-play is there. It's more of a mobile game, focused on dragging — inspired by Threes. I'm looking to see if its any fun. Let me know!
+
+I've always wanted to make a video game. This could be the one. My previous attempts never got past isolated demos because they were aiming for bigger ideas. They grew complex and unwieldy. So this game is designed to be simple. A game that I can actually make.
+
+-------------------------------------------------------------------------------
+
+Copyright (c) 2019 by Dave DeSandro (https://codepen.io/desandro/pen/ezNawy)
+
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+-->
 
 <!DOCTYPE html>
 <html lang="en" >
