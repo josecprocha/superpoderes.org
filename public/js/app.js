d6e87da8166d62c95d91ec9550f8fdb1b0b9dcef
José Cândido Pereira da Rocha <josecprocha@gmail.com>
2019-05-15 09:03:59 -0300

    Add Swinepeeper game

--- public/js/app.js
+++ public/js/app.js
@@ -49444,15 +49444,15 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ 0:
 /*!************************************************************************************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/third_party_html5_hanoi.scss ./resources/sass/third_party_html5_platform.scss ***!
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/third_party_html5_platform.scss ./resources/sass/third_party_html5_hanoi.scss ***!
   \************************************************************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! E:\Programação\Projeto ESP\superpoderes\resources\js\app.js */"./resources/js/app.js");
 __webpack_require__(/*! E:\Programação\Projeto ESP\superpoderes\resources\sass\app.scss */"./resources/sass/app.scss");
-__webpack_require__(/*! E:\Programação\Projeto ESP\superpoderes\resources\sass\third_party_html5_hanoi.scss */"./resources/sass/third_party_html5_hanoi.scss");
-module.exports = __webpack_require__(/*! E:\Programação\Projeto ESP\superpoderes\resources\sass\third_party_html5_platform.scss */"./resources/sass/third_party_html5_platform.scss");
+__webpack_require__(/*! E:\Programação\Projeto ESP\superpoderes\resources\sass\third_party_html5_platform.scss */"./resources/sass/third_party_html5_platform.scss");
+module.exports = __webpack_require__(/*! E:\Programação\Projeto ESP\superpoderes\resources\sass\third_party_html5_hanoi.scss */"./resources/sass/third_party_html5_hanoi.scss");
 
 
 /***/ })
--- routes/web.php
+++ routes/web.php
@@ -127,7 +127,6 @@ Route::get('third_party/html5/swinepeeper', function () {
   return view('third_party.html5.games.swinepeeper');
 });
 
-
 // Experiments
 
 // Fun
