3d2b57e93839a3bf258fdf497dcc381c8b16ef56
José Cândido Pereira da Rocha <josecprocha@gmail.com>
2019-05-31 16:10:56 -0300

    Configure ESLint, Pretier and AirBNB (JavaScript linter)

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+/**
+ * [Setting up ESLint on VS Code with Airbnb JavaScript Style Guide](https://travishorn.com/setting-up-eslint-on-vs-code-with-airbnb-javascript-style-guide-6eb78a535ba6)
+ * [Configuring ESLint](https://eslint.org/docs/user-guide/configuring)
+ * [Prettier - Integrating with Linters](https://prettier.io/docs/en/integrating-with-linters.html)
+ */
+
+module.exports = {
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error']
+  }
+};
+
+/*
+module.exports = {
+  plugins: ['prettier'],
+
+};
+*/
