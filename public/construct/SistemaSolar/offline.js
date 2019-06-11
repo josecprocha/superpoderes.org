e21139ddb1667bd9cf16db568b5d8b1ac7eb8362
José Cândido Pereira da Rocha <josecprocha@gmail.com>
2019-06-11 08:11:11 -0300

    Add old construct and unity games.

---
+++ public/construct/SistemaSolar/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1498004160,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/sprsol-sheet0.png",
+		"images/sprmercurio-sheet0.png",
+		"images/sprcamera-sheet0.png",
+		"images/sprsolbase-sheet0.png",
+		"images/sprsoltopo-sheet0.png",
+		"images/sprbackhud-sheet0.png",
+		"images/sprvenus-sheet0.png",
+		"images/sprterra-sheet0.png",
+		"images/sprlua-sheet0.png",
+		"images/sprmarte-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
