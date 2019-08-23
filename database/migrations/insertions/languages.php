2252bed08d752a3b117c97cf6036f1b6abd383c9
José Cândido Pereira da Rocha <josecprocha@gmail.com>
2019-08-22 21:18:37 -0300

    Improves the organization of the files needed to create the languages table at database/migration folder.

---
+++ database/migrations/insertions/languages.php
@@ -0,0 +1,236 @@
+<?php
+
+/**
+ * This file is part of "School of Super Powers" Project.
+ *
+ * @author (c) José Cândido Pereira da Rocha <josecprocha@gmail.com>
+ *
+ * This source file is subject to the {@license ISC} license that is bundled
+ * with this source code in the file LICENSE.md. Source is available in {@link
+ * https://github.com/josecprocha/superpowers}.
+ *
+ * Made with Laravel — {@see https://github.com/laravel/laravel to know more
+ * about this framework}.
+ */
+
+namespace Database\Migrations\Insertions; //educational politics;
+
+//------------------------------------------------------------------------------
+// Brazilian Kindergarten Skills defined by the "BNCC"
+//------------------------------------------------------------------------------
+
+require 'skills_meta_BR00_001.php';
+require 'skills_meta_BR00_002.php';
+require 'skills_meta_BR00_003.php';
+require 'skills_meta_BR00_004.php';
+require 'skills_meta_BR00_005.php';
+
+//------------------------------------------------------------------------------
+// Brazilian elementary School (Ensino Fundamental), BNCC
+//------------------------------------------------------------------------------
+
+// Portuguese (Língua Portuguesa)
+
+require 'skills_meta_BR00_006.php';
+require 'skills_meta_BR00_007.php';
+require 'skills_meta_BR00_008.php';
+require 'skills_meta_BR00_009.php';
+require 'skills_meta_BR00_010.php';
+require 'skills_meta_BR00_011.php';
+require 'skills_meta_BR00_012.php';
+require 'skills_meta_BR00_013.php';
+require 'skills_meta_BR00_014.php';
+require 'skills_meta_BR00_015.php';
+require 'skills_meta_BR00_016.php';
+require 'skills_meta_BR00_017.php';
+require 'skills_meta_BR00_018.php';
+require 'skills_meta_BR00_019.php';
+require 'skills_meta_BR00_020.php';
+require 'skills_meta_BR00_021.php';
+require 'skills_meta_BR00_022.php';
+require 'skills_meta_BR00_023.php';
+require 'skills_meta_BR00_024.php';
+require 'skills_meta_BR00_025.php';
+require 'skills_meta_BR00_026.php';
+require 'skills_meta_BR00_027.php';
+require 'skills_meta_BR00_028.php';
+require 'skills_meta_BR00_029.php';
+require 'skills_meta_BR00_030.php';
+require 'skills_meta_BR00_031.php';
+require 'skills_meta_BR00_032.php';
+require 'skills_meta_BR00_033.php';
+require 'skills_meta_BR00_034.php';
+require 'skills_meta_BR00_035.php';
+require 'skills_meta_BR00_036.php';
+require 'skills_meta_BR00_037.php';
+require 'skills_meta_BR00_038.php';
+require 'skills_meta_BR00_039.php';
+require 'skills_meta_BR00_040.php';
+require 'skills_meta_BR00_041.php';
+require 'skills_meta_BR00_042.php';
+require 'skills_meta_BR00_043.php';
+require 'skills_meta_BR00_044.php';
+require 'skills_meta_BR00_045.php';
+require 'skills_meta_BR00_046.php';
+require 'skills_meta_BR00_047.php';
+require 'skills_meta_BR00_048.php';
+require 'skills_meta_BR00_049.php';
+require 'skills_meta_BR00_050.php';
+require 'skills_meta_BR00_051.php';
+require 'skills_meta_BR00_052.php';
+require 'skills_meta_BR00_053.php';
+require 'skills_meta_BR00_054.php';
+require 'skills_meta_BR00_055.php';
+require 'skills_meta_BR00_056.php';
+require 'skills_meta_BR00_057.php';
+
+// English (Língua Inglesa)
+
+require 'skills_meta_BR00_058.php';
+require 'skills_meta_BR00_059.php';
+require 'skills_meta_BR00_060.php';
+require 'skills_meta_BR00_061.php';
+require 'skills_meta_BR00_062.php';
+require 'skills_meta_BR00_063.php';
+require 'skills_meta_BR00_064.php';
+require 'skills_meta_BR00_065.php';
+require 'skills_meta_BR00_066.php';
+require 'skills_meta_BR00_067.php';
+require 'skills_meta_BR00_068.php';
+require 'skills_meta_BR00_069.php';
+
+// Art (Arte)
+
+require 'skills_meta_BR00_070.php';
+require 'skills_meta_BR00_071.php';
+require 'skills_meta_BR00_072.php';
+require 'skills_meta_BR00_073.php';
+require 'skills_meta_BR00_074.php';
+require 'skills_meta_BR00_075.php';
+require 'skills_meta_BR00_076.php';
+require 'skills_meta_BR00_077.php';
+
+// Science (Ciência)
+
+require 'skills_meta_BR00_078.php';
+require 'skills_meta_BR00_079.php';
+require 'skills_meta_BR00_080.php';
+require 'skills_meta_BR00_081.php';
+require 'skills_meta_BR00_082.php';
+require 'skills_meta_BR00_083.php';
+require 'skills_meta_BR00_084.php';
+require 'skills_meta_BR00_085.php';
+require 'skills_meta_BR00_086.php';
+require 'skills_meta_BR00_087.php';
+require 'skills_meta_BR00_088.php';
+require 'skills_meta_BR00_089.php';
+require 'skills_meta_BR00_090.php';
+require 'skills_meta_BR00_091.php';
+
+// Physical Education (Educação Física)
+
+require 'skills_meta_BR00_092.php';
+require 'skills_meta_BR00_093.php';
+require 'skills_meta_BR00_094.php';
+require 'skills_meta_BR00_095.php';
+require 'skills_meta_BR00_096.php';
+require 'skills_meta_BR00_097.php';
+require 'skills_meta_BR00_098.php';
+require 'skills_meta_BR00_099.php';
+require 'skills_meta_BR00_100.php';
+
+// Religious Education (Ensino Religiosa)
+
+require 'skills_meta_BR00_101.php';
+require 'skills_meta_BR00_102.php';
+require 'skills_meta_BR00_103.php';
+require 'skills_meta_BR00_104.php';
+require 'skills_meta_BR00_105.php';
+require 'skills_meta_BR00_106.php';
+require 'skills_meta_BR00_107.php';
+require 'skills_meta_BR00_108.php';
+
+// Geography (Geografia)
+
+require 'skills_meta_BR00_109.php';
+require 'skills_meta_BR00_110.php';
+require 'skills_meta_BR00_111.php';
+require 'skills_meta_BR00_112.php';
+require 'skills_meta_BR00_113.php';
+require 'skills_meta_BR00_114.php';
+require 'skills_meta_BR00_115.php';
+require 'skills_meta_BR00_116.php';
+require 'skills_meta_BR00_117.php';
+require 'skills_meta_BR00_118.php';
+require 'skills_meta_BR00_119.php';
+require 'skills_meta_BR00_120.php';
+require 'skills_meta_BR00_121.php';
+require 'skills_meta_BR00_122.php';
+require 'skills_meta_BR00_123.php';
+
+// History (História)
+
+require 'skills_meta_BR00_124.php';
+require 'skills_meta_BR00_125.php';
+require 'skills_meta_BR00_126.php';
+require 'skills_meta_BR00_127.php';
+require 'skills_meta_BR00_128.php';
+require 'skills_meta_BR00_129.php';
+require 'skills_meta_BR00_130.php';
+require 'skills_meta_BR00_131.php';
+require 'skills_meta_BR00_132.php';
+require 'skills_meta_BR00_133.php';
+require 'skills_meta_BR00_134.php';
+require 'skills_meta_BR00_135.php';
+require 'skills_meta_BR00_136.php';
+require 'skills_meta_BR00_137.php';
+require 'skills_meta_BR00_138.php';
+require 'skills_meta_BR00_139.php';
+require 'skills_meta_BR00_140.php';
+require 'skills_meta_BR00_141.php';
+
+// Math (Matemática)
+
+require 'skills_meta_BR00_142.php';
+require 'skills_meta_BR00_143.php';
+require 'skills_meta_BR00_144.php';
+require 'skills_meta_BR00_145.php';
+require 'skills_meta_BR00_146.php';
+require 'skills_meta_BR00_147.php';
+require 'skills_meta_BR00_148.php';
+require 'skills_meta_BR00_149.php';
+require 'skills_meta_BR00_150.php';
+require 'skills_meta_BR00_151.php';
+require 'skills_meta_BR00_152.php';
+require 'skills_meta_BR00_153.php';
+require 'skills_meta_BR00_154.php';
+require 'skills_meta_BR00_155.php';
+require 'skills_meta_BR00_156.php';
+require 'skills_meta_BR00_157.php';
+require 'skills_meta_BR00_158.php';
+require 'skills_meta_BR00_159.php';
+require 'skills_meta_BR00_160.php';
+require 'skills_meta_BR00_161.php';
+require 'skills_meta_BR00_162.php';
+require 'skills_meta_BR00_163.php';
+require 'skills_meta_BR00_164.php';
+require 'skills_meta_BR00_165.php';
+require 'skills_meta_BR00_166.php';
+
+// College (Ensino médio)
+
+require 'skills_meta_BR00_167.php';
+require 'skills_meta_BR00_168.php';
+require 'skills_meta_BR00_169.php';
+require 'skills_meta_BR00_170.php';
+require 'skills_meta_BR00_171.php';
+require 'skills_meta_BR00_172.php';
+require 'skills_meta_BR00_173.php';
+
+// College Portuguese (Português do Ensino Médio)
+
+require 'skills_meta_BR00_174.php';
+require 'skills_meta_BR00_175.php';
+require 'skills_meta_BR00_176.php';
+require 'skills_meta_BR00_177.php';
+require 'skills_meta_BR00_178.php';
