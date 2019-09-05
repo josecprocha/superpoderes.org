1d8250f193095267b8897424a7e440ee0cccdcac
José Cândido Pereira da Rocha <josecprocha@gmail.com>
2019-09-05 10:04:25 -0300

    Add sensible insertions in addresses table.

--- database/migrations/2019_04_15_000130_create_addresses_table.php
+++ database/migrations/2019_04_15_000130_create_addresses_table.php
@@ -13,7 +13,9 @@
  * about this framework}.
  */
 
-namespace Database\Migrations; //localization;
+namespace Database\Migrations;
+
+//localization;
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -33,15 +35,13 @@ class CreateAddressesTable extends Migration
             $table->bigIncrements('id');
             $table->string('line1'); // Street and number
             $table->string('line2'); // Neighborhood
-            // ISO 3166 country
-            $table->foreign('countries_id')->references('id')->on('countries')->default('76');
-            // Country division
+            $table->foreign('countries_id')->references('id')->on('countries')->default('BR');
             $table->foreign('countries_divisions_id')->references('id')->on('countries_divisions')->nullable();
             $table->foreign('districts_id')->references('id')->on('districts');
-            // https://en.wikipedia.org/wiki/Postal_code
-            $table->char('postal_code', 10);
+            $table->string('postal_code'); // https://en.wikipedia.org/wiki/Postal_code
             $table->timestamps();
         });
+        include 'sensible_insertions/addresses.php';
     }
 
     /**
