ae8e5173efd3ce8f7e1dcfda90d0bc357a0af587
José Cândido Pereira da Rocha <josecprocha@gmail.com>
2019-10-04 10:32:37 -0300

    Create 419 error page (expired page)

--- resources/lang/en/errors.php
+++ resources/lang/en/errors.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * This file is part of "School of Super Powers" Project.
+ *
+ * @author (c) José Cândido Pereira da Rocha <josecprocha@gmail.com>
+ *
+ * This source file is subject to the {@license ISC} license that is bundled
+ * with this source code in the file LICENSE.md. Source is available in {@link
+ * https://github.com/josecprocha/superpowers}.
+ *
+ * Made with Laravel — {@see https://github.com/laravel/laravel to know more
+ * about this framework}.
+ */
+
 return [
     'btn' => 'Go Home',
 
@@ -8,10 +21,13 @@ return [
 
     '404title' => 'Page Not Found',
     '404text' => 'Sorry, the page you are looking for could not be found.',
-    
+
+    '419title' => 'Page Expired',
+    '419text' => 'Sorry, your session has expired. Please refresh and try again.',
+
     '500title' => 'Server Error',
     '500text' => 'Whooops, something went wrong on our servers.',
-    
+
     '503title' => 'Be Right Back',
     '503text' => 'Sorry, we are doing some maintenance. Please check back soon.',
 ];
--- resources/lang/pt-BR/errors.php
+++ resources/lang/pt-BR/errors.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * This file is part of "School of Super Powers" Project.
+ *
+ * @author (c) José Cândido Pereira da Rocha <josecprocha@gmail.com>
+ *
+ * This source file is subject to the {@license ISC} license that is bundled
+ * with this source code in the file LICENSE.md. Source is available in {@link
+ * https://github.com/josecprocha/superpowers}.
+ *
+ * Made with Laravel — {@see https://github.com/laravel/laravel to know more
+ * about this framework}.
+ */
+
 return [
     'btn' => 'Voltar pra Página Inicial',
 
@@ -8,10 +21,13 @@ return [
 
     '404title' => 'Página Não Encontrada',
     '404text' => 'Sinto muito, a página que está procurando não foi encontrada.',
-    
+
+    '419title' => 'Página Expirada',
+    '419text' => 'Sinto muito, sua sessão expirou. Por favor, atualize e tente novamente.',
+
     '500title' => 'Erro no Servidor',
     '500text' => 'Opa, há algo de errado com nossos servidores.',
-    
+
     '503title' => 'Volte em alguns minutos',
     '503text' => 'Desculpe, estamos fazendo alguma manutenção. Por favor, volte em breve.',
 ];
