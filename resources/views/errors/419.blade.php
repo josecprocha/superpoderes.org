a272a1dfbf513f6d8b117069418057d04f4f71ec
José Cândido Pereira da Rocha <josecprocha@gmail.com>
2019-10-04 10:36:53 -0300

    Create 419 error page (expired page)

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -1,6 +1,6 @@
 <html lang="pt-br">
   <head>
-    <title>@lang('errors.403title')</title>
+    <title>@lang('errors.419title')</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
@@ -12,11 +12,11 @@
       <div class="w-full md:w-1/2 bg-white flex items-center justify-center">
         <div class="max-w-sm m-8">
           <div class="text-black text-5xl md:text-15xl font-black">
-            403
+            419
           </div>
           <div class="w-16 h-1 bg-purple-light my-3 md:my-6"></div>
           <p class="text-grey-darker text-2xl md:text-3xl font-light mb-8 leading-normal">
-              @lang('errors.403text')
+              @lang('errors.419text')
           </p>
           <a href="{{ url('/') }}">
             <button class="bg-transparent text-grey-darkest font-bold uppercase tracking-wide py-3 px-6 border-2 border-grey-light hover:border-grey rounded-lg">
